832ba06080e2a27d7e32aacd9baa66cffd4db4db
duongbui2002 <duongbuidinh593@gmail.com>
2022-12-30 00:45:20 +0700

    duong

--- src/modules/account/accounts.controller.ts
+++ src/modules/account/accounts.controller.ts
@@ -1,12 +1,12 @@
-import {Body, Controller, Get, Param, Post, Query, UseGuards} from '@nestjs/common';
-import {AccountsService} from "@modules/account/accounts.service";
-import {SetRolesDto} from "@modules/account/dto/set-role.dto";
-import {AuthGuard} from "@common/guards/auth.guard";
+import { Body, Controller, Delete, Get, Param, Post, Query, UseGuards } from "@nestjs/common";
+import { AccountsService } from "@modules/account/accounts.service";
+import { SetRolesDto } from "@modules/account/dto/set-role.dto";
+import { AuthGuard } from "@common/guards/auth.guard";
 import RoleGuard from "@common/guards/roles.guard";
 import Role from "@common/enums/role.enum";
-import {AccountDecorator} from "@common/decorators/account.decorator";
-import {AccountDocument} from "@modules/account/schemas/account.schema";
-import {PaginationParamsDto} from "@common/dto/pagination-params.dto";
+import { AccountDecorator } from "@common/decorators/account.decorator";
+import { AccountDocument } from "@modules/account/schemas/account.schema";
+import { PaginationParamsDto } from "@common/dto/pagination-params.dto";
 
 @Controller()
 export class AccountsController {
@@ -15,34 +15,45 @@ export class AccountsController {
 
   @UseGuards(AuthGuard)
   @UseGuards(RoleGuard(Role.ExecutiveBoard))
-  @Post('account/set-roles/:id')
-  async setRoles(@Param('id') id: string, @Body() setRolesDto: SetRolesDto) {
-    const account = await this.accountService.findOne({_id: id})
-    const accountWithNewRoles = await this.accountService.update({_id: id}, setRolesDto, {new: true})
+  @Post("account/set-roles/:id")
+  async setRoles(@Param("id") id: string, @Body() setRolesDto: SetRolesDto) {
+    const account = await this.accountService.findOne({ _id: id });
+    const accountWithNewRoles = await this.accountService.update({ _id: id }, setRolesDto, { new: true });
 
     return {
       data: accountWithNewRoles,
       success: true
-    }
+    };
   }
 
   @UseGuards(AuthGuard)
-  @Get('account/info')
+  @Get("account/info")
   async getInfo(@AccountDecorator() account: AccountDocument) {
     return {
       success: true,
       data: account
-    }
+    };
   }
 
   @UseGuards(AuthGuard)
   @UseGuards(RoleGuard(Role.ExecutiveBoard))
-  @Get('accounts')
+  @Get("accounts")
   async getAccounts(@Query() options: PaginationParamsDto) {
-    const {data, paginationOptions} = await this.accountService.findAll({}, options)
+    const { data, paginationOptions } = await this.accountService.findAll({}, options);
     return {
       data: data,
       paginationOptions
-    }
+    };
+  }
+
+  @UseGuards(AuthGuard)
+  @UseGuards(RoleGuard(Role.ExecutiveBoard))
+  @Delete("account/:id")
+  async delete(@Param("id") id: string) {
+    const result = await this.accountService.remove({ _id: id });
+    return {
+      message: `Delete account with ${id} successfully`,
+      success: true
+    };
   }
 }
